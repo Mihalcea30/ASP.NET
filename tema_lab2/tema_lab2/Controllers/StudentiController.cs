830288f257abe521ef7b462a5274d3103e96895f
Your Name <mihalcea696@gmail.com>
2023-10-13 19:23:29 +0300

    tema2_lab

---
+++ tema_lab2/tema_lab2/Controllers/StudentiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static tema_lab2.Studenti;
+
+namespace tema_lab2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+ 
+    public class StudentsController : ControllerBase
+        {
+            public static List<Studenti> students = new List<Studenti>
+        {
+            new Studenti { Id = 1, Name = "Mihai", Age = 50 },
+            new Studenti { Id = 1, Name = "Cosmin", Age = 40},
+            new Studenti { Id = 1, Name = "Tudor", Age = 30},
+        };
+
+            // endpoint 
+            // Get 
+            [HttpGet]
+            public List<Studenti> getAllOrdered()
+            {
+                return students.OrderBy(s => s.Name).ToList();
+            }
+            // delete
+            [HttpDelete]
+            public List<Studenti> Delete(Studenti student)
+            {
+                var studentIndex = students.FindIndex(x => x.Id == student.Id);
+                students.RemoveAt(studentIndex);
+                return students;
+            }
+    }
+    }
---
+++ tema_lab2/tema_lab2/Studenti.cs
@@ -0,0 +1,9 @@
+﻿namespace tema_lab2
+{
+    public class Studenti
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
