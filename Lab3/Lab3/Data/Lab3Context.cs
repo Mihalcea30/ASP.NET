1cc2ff153f8250fee34c7a039b3bb49e297e9850
Your Name <mihalcea696@gmail.com>
2023-10-27 19:53:13 +0300

    tema3

---
+++ Lab3/Lab3/Data/Lab3Context.cs
@@ -0,0 +1,13 @@
+﻿using Lab3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab3.Data
+{
+    public class Lab3Context
+    {
+        public DbSet<Masina> Cars { get; set; }
+        public Lab3Context(DbContextOptions<Lab3Context> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
---
+++ Lab3/Lab3/Models/Masina.cs
@@ -0,0 +1,11 @@
+﻿
+using Lab3_23.Models.Base;
+
+namespace Lab3.Models
+{
+    public class Masina : BaseEntity
+    {
+        public string? Nume { get; set; }
+        public int AnFabricatie { get; set; }
+    }
+}
